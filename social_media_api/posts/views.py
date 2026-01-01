b3208dad56dacef51ec00daad34c9caff2b975e2
WanjakeKibe <wanjakejohn63@gmail.com>
2026-01-01 04:25:43 +0300

    feed functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -6,6 +6,27 @@ from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
 
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .models import Post
+from .serializers import PostSerializer
+
+
+class FeedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        following_users = request.user.following.all()
+
+        posts = Post.objects.filter(
+            author__in=following_users
+        ).order_by('-created_at')
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
