7859fd570be3cebec8ec2d227f6d74b32e00769f
WanjakeKibe <wanjakejohn63@gmail.com>
2026-01-03 14:00:40 +0300

    url config for like

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedView
+from .views import like_post, unlike_post
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
@@ -10,4 +11,5 @@ urlpatterns = router.urls
 
 urlpatterns += [
     path('feed/', FeedView.as_view(), name='user-feed'),
+    path('posts/<int:pk>/unlike/', unlike_post, name='unlike-post'),
 ]
